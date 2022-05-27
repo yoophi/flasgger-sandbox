78f551fd4297e7a74dfd5f0fffabf2814a75f922
Pyunghyuk Yoo <yoophi@gmail.com>
2022-05-27 20:17:21 +0900

    ✨ ADD: basic example

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,62 @@
 from flask import Flask, jsonify
 
+from app.extensions import swagger
+
 
 def create_app() -> Flask:
     app = Flask(__name__)
-    init_app(app)
+    init_extensions(app)
+    init_bp(app)
 
     return app
 
 
-def init_app(app: Flask):
+def init_extensions(app: Flask):
+    swagger.init_app(app)
+
+
+def init_bp(app: Flask):
     @app.route("/")
     def index():
         return jsonify({})
+
+    @app.route('/colors/<palette>')
+    def colors(palette):
+        """Example endpoint returning a list of colors by palette
+        This is using docstrings for specifications.
+        ---
+        parameters:
+          - name: palette
+            in: path
+            type: string
+            enum: ['all', 'rgb', 'cmyk']
+            required: true
+            default: all
+        definitions:
+          Palette:
+            type: object
+            properties:
+              palette_name:
+                type: array
+                items:
+                  $ref: '#/definitions/Color'
+          Color:
+            type: string
+        responses:
+          200:
+            description: A list of colors (may be filtered by palette)
+            schema:
+              $ref: '#/definitions/Palette'
+            examples:
+              rgb: ['red', 'green', 'blue']
+        """
+        all_colors = {
+            'cmyk': ['cyan', 'magenta', 'yellow', 'black'],
+            'rgb': ['red', 'green', 'blue']
+        }
+        if palette == 'all':
+            result = all_colors
+        else:
+            result = {palette: all_colors.get(palette)}
+
+        return jsonify(result)
